904c07344ef86951c0e29ace2e8bbc2e86413888
VanesStalkers <ivan.mironov@bk.ru>
2025-11-04 19:56:53 +0300

    added AuthForm component

--- front/Lobby.vue
+++ front/Lobby.vue
@@ -1,36 +1,68 @@
 <template>
-  <div id="lobby" :class="[
-    state.isMobile ? 'mobile-view' : '',
-    state.isLandscape ? 'landscape-view' : 'portrait-view',
-    !state.currentUser ? 'need-auth' : '',
-  ]">
-    <div class="main-logo">
-      <div class="contact-icons-wrapper">
-        <a href="https://t.me/smartgamesstudio" target="_black" class="telegram-link"> </a>
-        <a href="https://vk.com/smartgames.studio" target="_black" class="vk-link"> </a>
-      </div>
-    </div>
+  <div>
+    <auth-form
+      v-if="!state.currentUser"
+      :on-success="handleAuthSuccess"
+      :on-error="handleAuthError"
+      :call-lobby-enter="callLobbyEnter"
+      :custom-init-session="customInitSession"
+    />
 
-    <img id="bg-img" src="./assets/lobby.png" usemap="#image-map" :style="{
-      position: 'absolute',
-      left: `${bg.left || 0}px`,
-      top: `${bg.top || 0}px`,
-      scale: bg.scale || 1,
-      transformOrigin: 'center',
-      filter: 'grayscale(1)',
-    }" />
+    <div
+      id="lobby"
+      :class="[
+        state.isMobile ? 'mobile-view' : '',
+        state.isLandscape ? 'landscape-view' : 'portrait-view',
+        !state.currentUser ? 'need-auth' : '',
+      ]"
+    >
+      <div class="main-logo">
+        <div class="contact-icons-wrapper">
+          <a
+            href="https://t.me/smartgamesstudio"
+            target="_black"
+            class="telegram-link"
+          >
+          </a>
+          <a
+            href="https://vk.com/smartgames.studio"
+            target="_black"
+            class="vk-link"
+          >
+          </a>
+        </div>
+      </div>
+
+      <img
+        id="bg-img"
+        src="./assets/lobby.png"
+        usemap="#image-map"
+        :style="{
+          position: 'absolute',
+          left: `${bg.left || 0}px`,
+          top: `${bg.top || 0}px`,
+          scale: bg.scale || 1,
+          transformOrigin: 'center',
+          filter: 'grayscale(1)',
+        }"
+      />
+    </div>
   </div>
 </template>
 
 <script>
+import authForm from "./components/AuthForm.vue";
+
 export default {
-  components: {},
+  components: {
+    authForm,
+  },
   props: {
     customInitSession: Function,
   },
   data() {
     return {
-      auth: { login: '', password: '', err: null },
+      auth: { login: "", password: "", err: null },
       bg: {
         top: 0,
         left: 0,
@@ -59,30 +91,38 @@ export default {
     },
   },
   methods: {
-    async initSession(config = {}) {
-      await this.$root.initSession(config, {
-        success: async ({ lobbyId, availableLobbies }) => {
-          if (lobbyId) {
-            this.$set(this.$root.state, 'currentLobby', lobbyId);
-            this.lobbyDataLoaded = true;
-          } else {
-            if (availableLobbies.length) await this.callLobbyEnter({ lobbyId: availableLobbies[0] });
-          }
-        },
-        error: async (err) => {
-          if (err?.message) this.auth.err = err.message;
-          // чтобы пользователь увидел форму авторизации
-          this.lobbyDataLoaded = true;
-        },
-      });
+    async handleAuthSuccess({ lobbyId, availableLobbies }) {
+      if (lobbyId) {
+        // ??? как будто lobbyId всегда пустое
+        this.$set(this.$root.state, "currentLobby", lobbyId);
+        // this.lobbyDataLoaded = true;
+      } else {
+        if (availableLobbies.length)
+          await this.callLobbyEnter({ lobbyId: availableLobbies[0] });
+      }
+    },
+    async handleAuthError(err) {
+      // this.lobbyDataLoaded = true;
+    },
+    async callLobbyEnter({ lobbyId }) {
+      await api.action
+        .call({ path: "lobby.api.enter", args: [{ lobbyId }] })
+        .then((data) => {
+          this.$set(this.$root.state, "currentLobby", lobbyId);
+          // this.lobbyDataLoaded = true;
+          if (data.restoreGame) this.gameRestoreProcess = true;
+        })
+        .catch(prettyAlert);
     },
   },
   async created() {
     this.state.emit.joinGame = (data) => {
       const { deckType, gameType, gameId } = data;
-      app.$router.push({ path: `/game/${deckType}/${gameType}/${gameId}` }).catch((err) => {
-        console.log(err);
-      });
+      app.$router
+        .push({ path: `/game/${deckType}/${gameType}/${gameId}` })
+        .catch((err) => {
+          console.log(err);
+        });
     };
     this.state.emit.leaveGame = () => {
       if (document.fullscreenElement) document.exitFullscreen();
@@ -93,17 +133,14 @@ export default {
   },
   async mounted() {
     if (this.state.currentUser && this.state.currentLobby) {
-      this.lobbyDataLoaded = true;
-    } else {
-      const initSession = this.customInitSession || this.initSession;
-      initSession();
+      // this.lobbyDataLoaded = true;
     }
   },
-  async beforeDestroy() { },
+  async beforeDestroy() {},
 };
 </script>
 <style lang="scss">
-#lobby>.main-logo {
+#lobby > .main-logo {
   z-index: 1;
   position: absolute;
   width: 400px;
@@ -113,7 +150,6 @@ export default {
   background-image: url(./assets/logo.png);
   background-size: cover;
   transform-origin: top;
-
 }
 
 .contact-icons-wrapper {
@@ -146,7 +182,7 @@ export default {
   }
 }
 
-#lobby.mobile-view>.main-logo {
+#lobby.mobile-view > .main-logo {
   width: 300px;
   height: 150px;
   left: calc(50% - 150px);
@@ -157,7 +193,7 @@ export default {
   }
 }
 
-#lobby.mobile-view.landscape-view>.main-logo {
+#lobby.mobile-view.landscape-view > .main-logo {
   left: auto;
   right: 10px;
   top: -25px;
---
+++ front/components/AuthForm.vue
@@ -0,0 +1,218 @@
+<template>
+  <div v-if="showAuthForm" class="auth">
+    <div class="form">
+      <h3>Вход в лобби</h3>
+      <button class="new" v-on:click="createDemoUser">Войти как гость</button>
+      <button v-if="!showLoginForm" class="link" v-on:click="toggleLoginForm">
+        Войти с паролем
+      </button>
+      <template v-else>
+        <div class="inputs">
+          <input
+            v-model="auth.login"
+            name="login"
+            placeholder="логин"
+            @input="updateAuth"
+          />
+          <span :style="{ width: '10px' }"></span>
+          <input
+            v-model="auth.password"
+            name="password"
+            placeholder="пароль"
+            @input="updateAuth"
+          />
+        </div>
+        <button v-on:click="login">Авторизоваться</button>
+      </template>
+      <div v-if="auth.err" class="err">{{ auth.err }}</div>
+      <br />
+      <button
+        class="link"
+        style="color: white"
+        v-on:click="
+          createDemoUser({
+            tutorial: {
+              tutorial: 'lobby-tutorial-sales',
+              step: 'teambuilding',
+            },
+          })
+        "
+      >
+        Меня интересуют корпоративные игры
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AuthForm",
+  props: {
+    onSuccess: {
+      type: Function,
+      default: null,
+    },
+    onError: {
+      type: Function,
+      default: null,
+    },
+    callLobbyEnter: {
+      type: Function,
+      default: null,
+    },
+    customInitSession: {
+      type: Function,
+      default: null,
+    },
+  },
+  data() {
+    return {
+      showAuthForm: false,
+      showLoginForm: false,
+      auth: { login: "", password: "", err: null },
+    };
+  },
+  methods: {
+    updateAuth() {
+      // Сбрасываем ошибку при изменении полей
+      if (this.auth.err) {
+        this.auth.err = null;
+      }
+    },
+    toggleLoginForm() {
+      this.showLoginForm = true;
+    },
+    async initSession(config = {}) {
+      await this.$root.initSession(config, {
+        success: async ({ lobbyId, availableLobbies }) => {
+          this.showAuthForm = false;
+
+          if (this.onSuccess) {
+            await this.onSuccess({ lobbyId, availableLobbies });
+          } else {
+            // Дефолтная обработка успеха
+            if (lobbyId) {
+              this.$set(this.$root.state, "currentLobby", lobbyId);
+              this.$emit("session-success");
+            } else {
+              if (availableLobbies.length && this.callLobbyEnter) {
+                await this.callLobbyEnter({ lobbyId: availableLobbies[0] });
+              }
+            }
+          }
+        },
+        error: async (err) => {
+          this.showAuthForm = true;
+
+          if (err?.message) {
+            this.auth.err = err.message;
+          }
+          if (this.onError) {
+            await this.onError(err);
+          } else {
+            this.$emit("session-error", err);
+          }
+        },
+      });
+    },
+    async createDemoUser({ tutorial } = {}) {
+      await this.initSession({ demo: true, tutorial });
+    },
+    async login() {
+      await this.initSession({
+        login: this.auth.login,
+        password: this.auth.password,
+      });
+    },
+  },
+  async mounted() {
+    if (this.customInitSession) {
+      await this.customInitSession();
+    } else {
+      await this.initSession();
+    }
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.auth {
+  z-index: 10001;
+  position: fixed;
+  left: 0px;
+  top: 0px;
+  width: 100%;
+  height: 100%;
+  background-image: url(@/assets/clear-black-back.png);
+  background-size: cover;
+  display: grid;
+
+  > .form {
+    align-self: center;
+    justify-self: center;
+    width: 400px;
+    height: auto;
+    border: 4px solid #f4e205;
+    display: flex;
+    flex-wrap: wrap;
+    color: #f4e205;
+    max-width: 90%;
+    position: fixed;
+
+    > .err {
+      width: 100%;
+      color: orangered;
+      margin-bottom: 10px;
+    }
+
+    > h3 {
+      width: 100%;
+      text-align: center;
+    }
+
+    > .inputs {
+      width: 100%;
+      display: flex;
+      margin: 10px;
+
+      > input {
+        width: 50%;
+        font-size: 14px;
+        padding: 2px 8px;
+        background: transparent;
+        border: 2px solid #f4e205;
+        color: #f4e205;
+      }
+    }
+
+    > button {
+      width: 100%;
+      margin: 10px;
+      background: transparent;
+      color: #f4e205;
+      border: 2px solid #f4e205;
+      cursor: pointer;
+
+      &:hover {
+        opacity: 0.7;
+      }
+
+      &.new {
+        background-color: #f4e205;
+        color: black;
+      }
+
+      &.link {
+        background: transparent;
+        border: none;
+        text-decoration: underline;
+        font-size: 16px;
+        padding: 0;
+        margin: 5px 0;
+        font-size: 12px;
+        padding-bottom: 10px;
+      }
+    }
+  }
+}
+</style>
