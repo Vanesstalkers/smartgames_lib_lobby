41f9441c3475011ef2f08ee60e2128be3a1e20f8
VanesStalkers <ivan.mironov@bk.ru>
2026-03-25 01:46:25 +0300

    (-) menuGame.steps.teams

--- tutorial/menuGame.js
+++ tutorial/menuGame.js
@@ -51,27 +51,6 @@
           css: { boxShadow: '0 0 10px 10px white', padding: '4px 10px' },
         },
       ],
-      buttons: [{ text: 'Дальше', step: 'teams' }],
-    },
-    teams: {
-      text: `
-        Это иконка для открытия список команд (<a>только у корпоративных игр</a>). Можно присоединиться к любой команде на выбор.
-      `,
-      active: [
-        { selector: '.game-list-container .tutorial-games', css: { boxShadow: 'none' } },
-        { selector: '.game-list-container .no-games-label', css: { display: 'none' } },
-        { selector: '.game-list-container .show-teams', css: { boxShadow: '0 0 20px 10px white' } },
-      ],
-      actions: {
-        before: async (data) => {
-          const { $root, utils } = data;
-
-          await utils.showGamesBlock(data);
-
-          const $showTeamsBtn = $root.querySelector('.game-list-container .show-teams');
-          if (!$showTeamsBtn.classList.contains('open')) $showTeamsBtn.click();
-        },
-      },
       buttons: [{ text: 'Дальше', step: 'join' }],
     },
     join: {
